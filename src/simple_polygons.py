b6d44696ac4e148540c4fbb55fc6e1a854281955
Chris Havlin <chris.havlin@gmail.com>
2016-11-23 18:22:40 -0700

    added license to simple_polygons.py

--- src/simple_polygons.py
+++ src/simple_polygons.py
@@ -1,5 +1,20 @@
 """
+simple_polygons.py 
+
 Uses shapely and descartes to plot some simple polygons
+
+Copyright (C) 2016  Chris Havlin, <https://chrishavlin.wordpress.com>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import matplotlib.pyplot as plt
 import numpy as np
